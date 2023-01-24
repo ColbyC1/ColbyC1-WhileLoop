96b5e86dac7349e58f901288dc9f6a27831a9fc2
Colby Canselo <colbycanselo@gmail.com>
2023-01-24 17:24:56 +0000

    While Loop

--- src/main/java/WhileLoop.java
+++ src/main/java/WhileLoop.java
@@ -32,11 +32,15 @@ public class WhileLoop {
      *            (exclusive - for example, when end is 5, we calculate 0+1+2+3+4)
      *
      **/
+
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
 
-//        code here
+        while(i < end) {
+            sum+=i;
+            i++;
+        }
 
         return sum;
     }
